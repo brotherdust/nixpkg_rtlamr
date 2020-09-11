cb0a8cc0e5b96829a76a41eb6288e1b28834d2a3
Jarad Olson <brotherdust@gmail.com>
2020-09-11 14:27:43 -0600

    nixfmt changes

--- default.nix
+++ default.nix
@@ -1,1 +1,13 @@
-(import ./nix {}).ci
+{ buildGoPackage, fetchFromGitHub }:
+buildGoPackage rec {
+  name = "rtlamr-${version}";
+  version = "0.9.1";
+  goPackagePath = "github.com/bemasher/rtlamr";
+  src = fetchFromGitHub {
+    owner = "bemasher";
+    repo = "rtlamr";
+    rev = "4752d6d97cebdeadeeecda2e91fd5e3217140098";
+    sha256 = "13i1kq24043nmr3bqrpbg1k49a1vjqrzcxqaaxranyimkdnpxhwv";
+  };
+  goDeps = ./deps.nix;
+}
